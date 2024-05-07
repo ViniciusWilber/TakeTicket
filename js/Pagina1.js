01e7c0261d89ea63bdb946032dd484a77cebda78
Vinicius Wilber <141188078+ViniciusWilber@users.noreply.github.com>
2024-05-07 19:45:34 -0300

    alteracao do javascrpt

--- js/Pagina1.js
+++ js/Pagina1.js
@@ -9,24 +9,4 @@ $(document).ready(()=>{
         $('.cartasFerias').hide();
         $('.cartasAtracoes').hide();
     });
-    $('#click2').click(() =>{
-        $('.cartasExposicoes').hide();
-        $('.cartasMuseus').show();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
-    $('#click3').click(() =>{
-        $('.cartasExposicoes').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').show();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
 });
