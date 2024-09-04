0d104ba425a6a218b3c9a05be4d10ba7b4d55f27
Vinicius Wilber <141188078+ViniciusWilber@users.noreply.github.com>
2024-09-03 21:54:41 -0300

    correçoes

--- footer.php
+++ footer.php
@@ -7,28 +7,31 @@
 </head>
 <body>
     <main>
-        <footer>
-            <div>
-                <a href=""><i class=""></i>Central de ajuda</a>
-                <a href=""><i class=""></i>Opções de cancelamento</a>
-                <a href=""><i class=""></i>Duvidas frequentes</a>
-                <a href=""><i class=""></i>Apoio a pessoa com deficiencia</a>
-            </div>
-            <div>
-                <a href=""><i class=""></i></a>
-                <a href=""><i class=""></i>Carreiras</a>
-                <a href=""><i class=""></i>Investidores</a>
-                <a href=""><i class=""></i>Anuncies seu evento na TAKETICKET</a>
-            </div>
-            <div>
-                <h3>Horario de atendimento</h3>
-                <p>Seg.: À Sex.: 09:00 Ás 17:00</p>
-                <a href=""><i class=""></i>TakeTicket@gmail.com</a>
-            </div>
-            <section class="termos">
-                <p>2024 TakeTiket, inc.  .Privacidade  .Termos .Informaçãoes da empresa.</p>
-            </section>
-        </footer>
+        <!-- Conteúdo principal da página -->
     </main>
+
+    <footer>
+        <div class="conteudo">
+            <a href=""><i class=""></i>Central de ajuda</a>
+            <a href=""><i class=""></i>Opções de cancelamento</a>
+            <a href=""><i class=""></i>Duvidas frequentes</a>
+            <a href=""><i class=""></i>Apoio a pessoa com deficiência</a>
+        </div>
+        <div class="conteudo">
+            <a href=""><i class=""></i></a>
+            <a href=""><i class=""></i>Carreiras</a>
+            <a href=""><i class=""></i>Investidores</a>
+            <a href=""><i class=""></i>Anuncie seu evento na TAKETICKET</a>
+        </div>
+        <div class="conteudo">
+            <h3>Horário de atendimento</h3>
+            <p>Seg.: À Sex.: 09:00 às 17:00</p>
+            <a href=""><i class=""></i>TakeTicket@gmail.com</a>
+        </div>
+    </footer>
+
+    <div class="fundo">
+        <p>2024 TakeTicket, inc.  .Privacidade  .Termos .Informações da empresa.</p>
+    </div>
 </body>
 </html>
--- js/Pagina1.js
+++ js/Pagina1.js
@@ -1,100 +1,38 @@
-$(document).ready(()=>{
-    $('#click1').click(() =>{
-        $('.cartas').fadeIn('slow');
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
+$(document).ready(() => {
+    const sections = ['cartas', 'cartasMuseus', 'cartasSociais', 'cartasExcursoes', 'cartasTrilhas', 'cartasViagens', 'cartasFerias', 'cartasAtracoes'];
+    
+    // Função para mostrar a seção correspondente
+    function mostrarSeção(index) {
+        sections.forEach((section, i) => {
+            if (i === index) {
+                $('.' + section).fadeIn('slow');
+            } else {
+                $('.' + section).hide();
+            }
+        });
+    }
+
+    // Adiciona evento de clique para cada botão
+    sections.forEach((section, index) => {
+        $('#click' + (index + 1)).click(() => {
+            mostrarSeção(index);
+        });
     });
-    $('#click2').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').fadeIn('slow');
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
+
+    $('#aceitar').click(() => {
+        $('#cookies-msg').slideToggle(); 
     });
-    $('#click3').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').fadeIn('slow');
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
-    $('#click4').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').fadeIn('slow');
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
-    $('#click5').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').fadeIn('slow');
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
-    $('#click6').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').fadeIn('slow');
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').hide();
-    });
-    $('#click7').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').fadeIn('slow');
-        $('.cartasAtracoes').hide();
-    });
-    $('#click8').click(() =>{
-        $('.cartas').hide();
-        $('.cartasMuseus').hide();
-        $('.cartasSociais').hide();
-        $('.cartasExcursoes').hide();
-        $('.cartasTrilhas').hide();
-        $('.cartasViagens').hide();
-        $('.cartasFerias').hide();
-        $('.cartasAtracoes').fadeIn('slow');
+
+    // Seleciona todos os botões
+    const botoes = document.querySelectorAll('.meu-botao');
+
+    // Adiciona um ouvinte de evento para cada botão
+    botoes.forEach(botao => {
+        botao.addEventListener('click', () => {
+            // Remove a classe 'clicado' de todos os botões
+            botoes.forEach(b => b.classList.remove('clicado'));
+            // Adiciona a classe 'clicado' ao botão clicado
+            botao.classList.add('clicado');
+        });
     });
 });
-$('#aceitar').click(()=>{
-    $('#cookies-msg').slideToggle(); 
-});
-
-  // Seleciona todos os botões
-  var botoes = document.querySelectorAll('.meu-botao');
-
-  // Adiciona um ouvinte de evento para cada botão
-  botoes.forEach(function(botao) {
-    botao.addEventListener('click', function() {
-      // Remove a classe 'clicado' de todos os botões
-      botoes.forEach(function(b) {
-        b.classList.remove('clicado');
-      });
-      // Adiciona a classe 'clicado' ao botão clicado
-      botao.classList.add('clicado');
-    });
-  });
