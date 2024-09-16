00474ace127626b18b0c48edba75112690296da9
Clarissa Dias <163210223+clarissandias@users.noreply.github.com>
2024-09-16 20:31:53 -0300

    alteração da fonte!

--- cadastroevento.php
+++ cadastroevento.php
@@ -19,9 +19,12 @@
                 <div class="section1">
                     <h3>1. Onde o seu evento vai acontecer?</h3>
 
-                    <div class="primeiro">
+
+                    <!-- Campos para endereço -->
+                    <input type="text" id="endereco" name="endereco" class="input-box" placeholder="Endereço">
+
                     <div class="info">
-                        <label for="nome-local">Rua</label>
+                        <label for="nome-local">Nome do Local</label>
                         <input type="text" id="nome-local" name="nome-local" class="input-box" maxlength="100"
                             placeholder="Nome do Local">
                     </div>
@@ -36,12 +39,12 @@
                         <label for="rua">Logradouro</label>
                         <input type="text" id="rua" name="rua" class="input-box" placeholder="Rua/Avenida">
                     </div>
-                
+                </div>
                 <div class="info">
                     <label for="numero">Número</label>
                     <input type="text" id="numero" name="numero" class="input-box" placeholder="Número">
                 </div>
-                </div>
+
                 <div class="info">
                     <label for="complemento">Complemento</label>
                     <input type="text" id="complemento" name="complemento" class="input-box" maxlength="250"
@@ -63,7 +66,6 @@
                     <label for="estado">Estado</label>
                     <input type="text" id="estado" name="estado" class="input-box" placeholder="Estado">
                 </div>
-                </div>
             </div>
 
             <div class="container">
@@ -78,6 +80,8 @@
                         <label for="imagem-divulgacao">Imagem de divulgação (opcional)</label>
                         <input type="file" id="imagem-divulgacao" accept=".jpg, .jpeg, .png, .gif"
                             aria-describedby="imagem-instrucao">
+                        <small id="imagem-instrucao">A dimensão recomendada é de 1600 x 838. Formato JPEG, GIF ou PNG de
+                            no máximo 2MB.</small>
 
                         <label for="assunto">Assunto *</label>
                         <select id="assunto" name="assunto" required>
@@ -104,26 +108,19 @@
                 </div>
             </div>
             <div class="container">
-
+                <div class="section">
                     <h3>4. Data e horário</h3>
-                    <div class="section1">
-
-                        <fieldset class="quadrado">
-
-                    <label for="data-inicio">Início *</label>
+                    <label for="data-inicio">Data de Início *</label>
                     <input type="date" id="data-inicio" class="input-box" value="2024-09-04">
-                    <input type="time" id="hora-inicio" class="input-box" value="19:30">
-    
-                    </fieldset>
-                    
-                        <fieldset class="quadrado">
 
-                    <label for="data-termino">Término *</label>
-                    <input type="date" id="data-inicio" class="input-box" value="2024-09-06">
+                    <label for="hora-inicio">Hora de Início *</label>
                     <input type="time" id="hora-inicio" class="input-box" value="19:30">
-                    </fieldset>
 
-                    </div>
+                    <label for="data-termino">Data de Término *</label>
+                    <input type="date" id="data-termino" class="input-box" value="2024-09-06">
+
+                    <label for="hora-termino">Hora de Término *</label>
+                    <input type="time" id="hora-termino" class="input-box" value="19:30">
                 </div>
             </div>
         </div>
