d85052f5073a58ba72534bea96767305b24edf35
lucas correia macena <163210665+lucascorreia123@users.noreply.github.com>
2024-09-17 17:01:15 -0300

    ajustando

--- cadastroevento.php
+++ cadastroevento.php
@@ -20,33 +20,30 @@
                 <section class="section1">
 
 
-                    <div class="primeiro">
+
                         <div class="info">
-                            <label for="rua">Logradouro</label>
+
                             <input type="text" id="rua" name="rua" class="input-box" placeholder="Rua/Avenida">
                         </div>
 
 
                         <div class="info">
-                            <label for="cep">CEP</label>
                             <input type="text" id="cep" name="cep" class="input-box" placeholder="CEP">
                         </div>
 
 
-
-
                         <div class="info">
-                            <label for="bairro">Bairro</label>
+
                             <input type="text" id="bairro" name="bairro" class="input-box" placeholder="Bairro">
                         </div>
-                    </div>
-                    <section>
+                </section>
+                <section class="section1">
                         <div class="info">
-                            <label for="numero">Número</label>
+
                             <input type="text" id="numero" name="numero" class="input-box" placeholder="Número">
                         </div>
                         <div class="info">
-                            <label for="complemento">Complemento</label>
+
                             <input type="text" id="complemento" name="complemento" class="input-box" maxlength="250"
                                 placeholder="Complemento">
                         </div>
@@ -55,24 +52,23 @@
 
 
                         <div class="info">
-                            <label for="cidade">Cidade </label>
+    
                             <input type="text" id="cidade" name="cidade" class="input-box" placeholder="Cidade">
                         </div>
 
                         <div class="info">
-                            <label for="estado">Estado</label>
+
                             <input type="text" id="estado" name="estado" class="input-box" placeholder="Estado">
                         </div>
-        
+                    </section>
 
         </fieldset>
 
         <fieldset class="container">
             <legend>Informações básicas</legend>
-            <div class="container">
+
                 <div class="section">
 
-                    <p>Adicione as principais informações do evento.</p>
                     <form>
                         <label for="nome-evento">Nome do evento *</label>
                         <input type="text" id="nome-evento" name="nome-evento" placeholder="Nome do evento" required
@@ -94,7 +90,7 @@
                             <!-- Adicione outras opções aqui -->
                         </select>
                     </form>
-                </div>
+      
             </div>
         </fieldset>
         <!-- Seção de Descrição do Evento -->
@@ -109,7 +105,7 @@
         <fieldset class="container">
             <legend>Data e horário</legend>
 
-
+            <div class="data">
             <fieldset class="quadrado">
 
                 <label for="data-inicio">Início *</label>
@@ -117,16 +113,32 @@
                 <input type="time" id="hora-inicio" class="input-box" value="19:30">
 
 
-
+                </fieldset>
                 <fieldset class="quadrado">
 
                     <label for="data-termino">Término *</label>
                     <input type="date" id="data-inicio" class="input-box" value="2024-09-06">
                     <input type="time" id="hora-inicio" class="input-box" value="19:30">
-                </fieldset>
+
 
 
             </fieldset>
+        </div>
+            <button class="animated-button">
+                <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                  ></path>
+                </svg>
+                <span class="text">Cadastrar</span>
+                <span class="circle"></span>
+                <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                  ></path>
+                </svg>
+              </button>
+        </fieldset>
 
             </div>
             <!-- Seção 5: Ingressos -->
